764e007fc6310e7782eb3e7a09236db154748ee9
Cam Tullos <>
2021-02-04 14:54:03 -0500

    peer deps fix?

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-02-04T16:25:59.266Z",
+    "time": "2021-02-04T19:37:54.637Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
